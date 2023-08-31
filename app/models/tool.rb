036eaa797f4eb94cc1cb1d0d2f20379e81891d37
wa-bha <bhavit.w04@gmail.com>
2023-08-31 14:24:29 +1200

    feat: add category

--- app/models/tool.rb
+++ app/models/tool.rb
@@ -1,4 +1,5 @@
 class Tool < ApplicationRecord
   has_many :user_tool_associations, dependent: :destroy
   has_many :users, through: :user_tool_associations
+  belongs_to :category
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_30_233917) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_31_022022) do
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "tools", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "category_id"
+    t.index ["category_id"], name: "index_tools_on_category_id"
   end
 
   create_table "user_tool_associations", force: :cascade do |t|
@@ -32,6 +40,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_30_233917) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "tools", "categories"
   add_foreign_key "user_tool_associations", "tools"
   add_foreign_key "user_tool_associations", "users"
 end
