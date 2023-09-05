199561f62bc4896e47b165de4e4e7e6565d129a0
wa-bha <bhavit.w04@gmail.com>
2023-09-05 16:22:40 +1200

    chore: cleanup remove original rails tests

--- test/controllers/tools_controller_test.rb
+++
@@ -1,8 +0,0 @@
-require "test_helper"
-
-class ToolsControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get tools_index_url
-    assert_response :success
-  end
-end
--- test/models/user_tool_association_test.rb
+++
@@ -1,7 +0,0 @@
-require "test_helper"
-
-class UserToolAssociationTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
